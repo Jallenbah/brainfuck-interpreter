1a15b2d7cdc15955a2e22006e7db1f3537bc43e5
Jallen <111014323+Jallenbah@users.noreply.github.com>
2024-06-08 19:17:47 +0100

    Prevent duplicating program in memory by using ReadOnlySpan instead of char array

--- code/brainfuck-interpreter/Brainfuck/Interpreter.cs
+++ code/brainfuck-interpreter/Brainfuck/Interpreter.cs
@@ -8,14 +8,14 @@ internal class Interpreter
     /// <param name="code">The brainfuck code to run</param>
     public static void Execute(string code)
     {
-        var codeAsArray = code.ToCharArray();
+        ReadOnlySpan<char> prog = code;
 
         var currentCell = new DataCell(); // The cell currently pointed to by the brainfuck program. 
         var loopIndexStack = new Stack<int>(); // A stack of loop start position indexes, used to know where to jump back to when the end of the loop body is reached
 
-        for (int pc = 0; pc < codeAsArray.Length; pc++)
+        for (int pc = 0; pc < prog.Length; pc++)
         {
-            switch (codeAsArray[pc])
+            switch (prog[pc])
             {
                 case '+':
                     currentCell.Data++;
@@ -39,23 +39,23 @@ internal class Interpreter
                     break;
             }
 
-            if (codeAsArray[pc] == '[')
+            if (prog[pc] == '[')
             {
                 loopIndexStack.Push(pc);
-                if (currentCell.Data == 0) // Current cell of 0 at loop start [ means skip loop
+                if (currentCell.Data == 0) // Current cell of 0 at loop start [ means skip loop, so we need to find the closing brace
                 {
                     // If there are nested loops, we can't just find the next ] so we need to know how many we need to skip by counting them
                     var closingBracesToSkip = 0;
 
                     // Find closing loop brace to jump to
                     bool foundClosingBrace = false;
-                    for (int i = pc + 1; i < codeAsArray.Length; i++) // Add 1 to pc for starting point to skip initial [
+                    for (int i = pc + 1; i < prog.Length; i++) // Add 1 to pc for starting point to skip initial [
                     {
-                        if (codeAsArray[i] == '[')
+                        if (prog[i] == '[')
                         {
                             closingBracesToSkip++; // Found the start of a nested loop, so we will need to skip a ]
                         }
-                        else if (codeAsArray[i] == ']')
+                        else if (prog[i] == ']')
                         {
                             if (closingBracesToSkip == 0) // We don't have a ] to skip and we've found a ] so that must be the end of our loop
                             {
@@ -74,7 +74,7 @@ internal class Interpreter
                     }
                 }
             }
-            else if (codeAsArray[pc] == ']')
+            else if (prog[pc] == ']')
             {
                 if (!loopIndexStack.Any())
                 {
